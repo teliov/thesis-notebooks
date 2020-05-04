2a09f230ca873656bf645379dcb9d4ee5b4a5bd8
Obinna Agba <obinna@prowork.me>
2020-05-04 06:11:36 +0200

    added sbatch for random forest grid search

--- zz_scripts/rf_grid_search/train.py
+++ zz_scripts/rf_grid_search/train.py
@@ -3,11 +3,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedShuffleSplit
 import json
 import os
-import joblib
 import argparse
 from timeit import default_timer as timer
 from thesislib.utils.ml import models, report
-import logging
+import pickle
+import sys
 
 
 class RFParams(object):
@@ -17,30 +17,39 @@ class RFParams(object):
     min_samples_split = 2,
     min_samples_leaf = 1,
     max_leaf_nodes = None,
-    min_impurity_decrease = 0.0
     max_features = 'auto'
+
+    def get_params(self):
+        return {
+            "n_estimators": self.n_estimators,
+            "criterion": self.criterion,
+            "max_depth": self.max_depth,
+            "min_samples_split": self.min_samples_split,
+            "min_samples_leaf": self.min_samples_leaf,
+            "max_leaf_nodes": self.max_leaf_nodes,
+            "max_features": self.max_features
+        }
+
+    def __str__(self):
+        params = self.get_params()
+        name = ""
+        for key, val in params.items():
+            name += "%s_%s_" % (key, str(val))
+        name = name.strip("_")
+        return name
 
 
 def train_rf(data_file, symptoms_db_json, output_dir, rfparams):
-    logger = report.Logger("Random Forest Classification on QCE")
-
     try:
-        logger.log("Starting Random Forest Classification")
         begin = timer()
         with open(symptoms_db_json) as fp:
             symptoms_db = json.load(fp)
             num_symptoms = len(symptoms_db)
 
-        logger.log("Reading CSV")
-        start = timer()
         df = pd.read_csv(data_file, index_col='Index')
-        end = timer()
-        logger.log("Reading CSV: %.5f secs" % (end - start))
 
         classes = df.LABEL.unique().tolist()
 
-        logger.log("Prepping Sparse Representation")
-        start = timer()
         label_values = df.LABEL.values
         ordered_keys = ['GENDER', 'RACE', 'AGE', 'SYMPTOMS']
         df = df[ordered_keys]
@@ -48,11 +57,6 @@ def train_rf(data_file, symptoms_db_json, output_dir, rfparams):
         sparsifier = models.ThesisSymptomSparseMaker(num_symptoms=num_symptoms)
         data_csc = sparsifier.fit_transform(df)
 
-        end = timer()
-        logger.log("Prepping Sparse Representation: %.5f secs" % (end - start))
-
-        logger.log("Shuffling Data")
-        start = timer()
         split_t = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
         train_data = None
         train_labels = None
@@ -64,37 +68,20 @@ def train_rf(data_file, symptoms_db_json, output_dir, rfparams):
             test_data = data_csc[test_index]
             test_labels = label_values[test_index]
 
-        end = timer()
-        logger.log("Shuffling Data: %.5f secs" % (end - start))
-
-        logger.log("Training RF Classifier")
-        start = timer()
         clf = RandomForestClassifier(
             n_estimators=rfparams.n_estimators,
             criterion=rfparams.criterion,
             max_depth=rfparams.max_depth,
             min_samples_split=rfparams.min_samples_split,
             min_samples_leaf=rfparams.min_samples_leaf,
-            min_weight_fraction_leaf=0.0,
-            max_features='auto',
-            max_leaf_nodes=None,
-            min_impurity_decrease=0.0,
-            min_impurity_split=None,
-            bootstrap=True,
-            oob_score=False,
-            n_jobs=2,
-            random_state=None,
-            verbose=0,
-            warm_start=False,
-            class_weight=None
+            max_features=rfparams.max_features,
+            max_leaf_nodes=rfparams.max_leaf_nodes,
+            n_jobs=2
         )
 
         clf = clf.fit(train_data, train_labels)
-        end = timer()
-        logger.log("Training RF Classifier: %.5f secs" % (end - start))
 
-        print("Calculating Accuracy")
-        start = timer()
+        train_time = timer()
 
         scorers = report.get_tracked_metrics(classes=classes, metric_name=[
             report.ACCURACY_SCORE,
@@ -103,55 +90,57 @@ def train_rf(data_file, symptoms_db_json, output_dir, rfparams):
         ])
 
         train_results = {
-            "name": "Naive Bayes Classifier",
+            "name": "RF %s" % rfparams.__str__(),
         }
 
         for key, scorer in scorers.items():
-            logger.log("Starting Score: %s" % key)
-            scorer_timer_train = timer()
             train_score = scorer(clf, train_data, train_labels)
-            scorer_timer_test = timer()
             test_score = scorer(clf, test_data, test_labels)
             train_results[key] = {
                 "train": train_score,
                 "test": test_score
             }
-            scorer_timer_end = timer()
-            train_duration = scorer_timer_test - scorer_timer_train
-            test_duration = scorer_timer_end - scorer_timer_test
-            duration = scorer_timer_end - scorer_timer_train
-            logger.log("Finished score: %s.\nTook: %.5f seconds\nTrain: %.5f, %.5f secs\n Test: %.5f, %.5f secs"
-                       % (key, duration, train_score, train_duration, test_score, test_duration))
 
-        end = timer()
-        logger.log("Calculating Accuracy: %.5f secs" % (end - start))
+        score_time = timer()
 
-        train_results_file = os.path.join(output_dir, "rf_train_results_sparse_grid_search.json")
+        clf = pickle.dumps(clf)
+        model_size =  sys.getsizeof(clf)
+        train_results["stats"] = {
+            "score_time": score_time - train_time,
+            "train_time": train_time - begin,
+            "model_size": model_size
+        }
+
+        train_results_file = os.path.join(output_dir, "rf_gsearch_%s.json" % rfparams.__str__())
         with open(train_results_file, "w") as fp:
             json.dump(train_results, fp)
 
-        estimator_serialized = {
-            "clf": clf,
-            "name": "random forest classifier on sparse"
-        }
-        estimator_serialized_file = os.path.join(output_dir, "rf_serialized_sparse_grid_search.joblib")
-        joblib.dump(estimator_serialized, estimator_serialized_file)
-
-        finish = timer()
-        logger.log("Completed Random Forest Classification: %.5f secs" % (finish - begin))
         res = True
-    except Exception as e:
-        message = e.__str__()
-        logger.log(message, logging.ERROR)
+    except Exception:
         res = False
 
     return res
 
 
 if __name__ == "__main__":
+    """
+    "n_estimators": self.n_estimators,
+            "criterion": self.criterion,
+            "max_depth": self.max_depth,
+            "min_samples_split": self.min_samples_split,
+            "min_samples_leaf": self.min_samples_leaf,
+            "max_leaf_nodes": self.max_leaf_nodes,
+            "max_features": self.max_features
+    """
     parser = argparse.ArgumentParser(description='Medvice RandomForest Trainer')
     parser.add_argument('--data', help='Path to train csv file')
     parser.add_argument('--symptoms_json', help='Path to symptoms db.json')
+    parser.add_argument('--max_depth', default=None)
+    parser.add_argument('--min_samples_split', type=int, default=2)
+    parser.add_argument('--min_samples_leaf', type=int, default=1)
+    parser.add_argument('--max_leaf_nodes', type=int, default=1)
+    parser.add_argument('--max_features', default="auto")
+    parser.add_argument('--estimators', default=100, type=int)
     parser.add_argument('--output_dir', help='Directory where results and trained model should be saved to')
 
     args = parser.parse_args()
@@ -168,4 +157,14 @@ if __name__ == "__main__":
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
 
-    train_rf(data_file=data_file, output_dir=output_dir, symptoms_db_json=symptoms_db_json)
+    rfparams = RFParams()
+    rfparams.max_depth = args.max_depath
+    rfparams.min_samples_split = args.min_samples_split
+    rfparams.min_samples_leaf = args.min_samples_leaf
+    rfparams.max_leaf_nodes = args.max_leaf_nodes
+    rfparams.max_features = args.max_features
+    rfparams.n_estimators = args.estimators
+
+    print("Running: %s" % rfparams.__str__())
+
+    train_rf(data_file=data_file, output_dir=output_dir, symptoms_db_json=symptoms_db_json, rfparams=rfparams)
