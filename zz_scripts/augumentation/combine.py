4d8f74f386d5db2ea563d37f135508f44b6b1351
Obinna Agba <obinna@prowork.me>
2020-08-03 09:09:11 +0200

    added script for combining relevant datasets into one for training / testing

---
+++ zz_scripts/augumentation/combine.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import os
+from sklearn.model_selection import StratifiedShuffleSplit
+import pathlib
+
+BASE_DIR="/home/oagba/bulk/data/"
+TARGET_SOURCES = {
+    "output_basic_10k": 20,
+    "output_basic_2_cnt_10k": 10,
+    "output_basic_3_cnt_10k": 10,
+    "output_basic_4_cnt_10k": 10,
+    "output_basic_avg_cnt_10k": 10,
+    "output_basic_pct_10_10k": 10,
+    "output_basic_pct_20_10k": 10,
+    "output_basic_pct_30_10k": 10,
+    "output_basic_pct_50_10k": 10,
+    "output_basic_pct_70_10k": 10,
+    "output_basic_inc_1_10k": 10,
+    "output_basic_inc_2_10k": 10,
+    "output_basic_inc_3_10k": 10
+}
+
+if __name__ == "__main__":
+
+    test_dfs = []
+    train_dfs = []
+
+    for dirname, pct in TARGET_SOURCES.items():
+
+        split_t = StratifiedShuffleSplit(n_splits=1, test_size=pct/100)
+
+        test_file = os.path.join(BASE_DIR, dirname, "symptoms/csv/parsed/test.csv_sparse.csv")
+        train_file = os.path.join(BASE_DIR, dirname, "symptoms/csv/parsed/train.csv_sparse.csv")
+
+        files = [test_file, train_file]
+
+        for idx in range(2):
+            df = pd.read_csv(files[idx], index_col="Index")
+            label_values = df.LABEL.values = df['LABEL']
+
+            for _, test_index in split_t.split(df, label_values):
+                df = df.iloc[test_index]
+
+            if idx == 0:
+                test_dfs.append(df)
+            else:
+                train_dfs.append(df)
+
+    opdir = os.path.join(BASE_DIR, "output_combined_15k", "symptoms/csv/parsed")
+    pathlib.Path(opdir).mkdir(exist_ok=True, parents=True)
+
+    # test
+    df = pd.concat(test_dfs)
+    filename = os.path.join(opdir, "test.csv_sparse.csv")
+    df.to_csv(filename)
+
+    # train
+    df = pd.concat(test_dfs)
+    filename = os.path.join(opdir, "train.csv_sparse.csv")
+    df.to_csv(filename)
