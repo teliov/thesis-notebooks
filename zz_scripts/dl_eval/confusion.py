0379d44bf4315b47b2ea149dbb71243f3e912578
Obinna Agba <obinna@prowork.me>
2020-08-18 17:06:14 +0200

    added script for generating mlp confusion matrix

--- zz_scripts/dl_eval/confusion.py
+++ zz_scripts/dl_eval/confusion.py
@@ -8,7 +8,6 @@ import pathlib
 import argparse
 
 import torch
-import torch.nn.functional as F
 import joblib
 
 AGE_MEAN = 38.741316816862515
@@ -18,8 +17,10 @@ NUM_SYMPTOMS = 376
 NUM_CONDITIONS = 801
 INPUT_DIM = 383
 
+from sklearn.metrics import confusion_matrix
 
-def confidence(state_dict_path, train_file_path, output_dir):
+
+def confusion(state_dict_path, train_file_path, output_dir):
     begin = timer()
     if not os.path.exists(state_dict_path):
         raise ValueError("Invalid state dict path passed")
@@ -42,36 +43,16 @@ def confidence(state_dict_path, train_file_path, output_dir):
     df = sparsifier.transform(df)
 
     with torch.no_grad():
-        labels = torch.LongTensor(labels)
         df = torch.FloatTensor(df.todense())
 
         out = model(df)
-        predicted_prob = F.softmax(out, dim=1)
-        sorted_predicted_prob, sort_idx = torch.sort(predicted_prob, dim=1, descending=True)
+        _, predicted_labels = torch.max(out, dim=1)
 
-        predicted_labels = sort_idx[:, 0]
+        predicted_labels = predicted_labels.numpy()
+        cnf_mat = confusion_matrix(labels.values, predicted_labels)
 
-        # for all the data
-        op_file = os.path.join(output_dir, "mlp_confidence_matrix_all.joblib")
-        joblib.dump(sorted_predicted_prob[:, :5].numpy(), op_file)
-
-        # when it;s correct
-        op_file = os.path.join(output_dir, "mlp_confidence_matrix_1.joblib")
-        joblib.dump(sorted_predicted_prob[predicted_labels == labels, :5].numpy(), op_file)
-
-        # when it's wrong
-        op_file = os.path.join(output_dir, "mlp_confidence_matrix_0.joblib")
-        joblib.dump(sorted_predicted_prob[predicted_labels != labels, : 5].numpy(), op_file)
-
-        # when it's top 5 ?
-        top_5 = sort_idx[:, :5]
-        combined = torch.sum(top_5 == labels.view(-1, 1), dim=1)
-        op_file = os.path.join(output_dir, "mlp_confidence_matrix_5.joblib")
-        joblib.dump(sorted_predicted_prob[combined == 1, : 5].numpy(), op_file)
-
-        # where it's not even top 5 accurate ?
-        op_file = os.path.join(output_dir, "mlp_confidence_matrix_n5.joblib")
-        joblib.dump(sorted_predicted_prob[combined == 0, : 5].numpy(), op_file)
+        op_file = os.path.join(output_dir, "mlp_confusion_matrix.joblib")
+        joblib.dump(cnf_mat, op_file)
 
     duration = timer() - begin
     print("Took : %.7f seconds" % duration)
@@ -91,4 +72,4 @@ if __name__ == "__main__":
     data_path = args.data_path
     output_dir = args.output_dir
 
-    confidence(state_dict_path, data_path, output_dir)
+    confusion(state_dict_path, data_path, output_dir)
